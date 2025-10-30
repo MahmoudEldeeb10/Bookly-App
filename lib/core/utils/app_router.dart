d78c8e1da7350838f5ef77555cf64a55ac3cd21b
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2025-10-30 19:18:06 +0300

    upload project

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,21 @@
+import 'package:bookly/features/home/presentaion/views/book_details_view.dart';
 import 'package:bookly/features/home/presentaion/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 // abstract because i will not craete copies from it
 abstract class AppRouter {
+  static const KHomeView = '/homeView';
+  static const KBookDetailsView = '/bookDetailsView';
+
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
-      GoRoute(path: '/homeView', builder: (context, state) => const HomeView()),
+      GoRoute(path: KHomeView, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: KBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
 }
--- lib/features/home/presentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentaion/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentaion/views/widgets/custom_book_detailsa_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -5,6 +6,11 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: const Column(
+        children: [SafeArea(child: CustomBookDetailsAppBar())],
+      ),
+    );
   }
 }
