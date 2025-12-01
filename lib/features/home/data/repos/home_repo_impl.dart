ca7b9feb6ea6d3cf61f2aef96a0cf0f4676e07e4
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2025-12-01 16:16:30 +0200

    finish home repo impl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -23,7 +23,7 @@ class HomeRepoImpl extends HomeRepo {
       }
 
       return Right(books);
-    } on Exception catch (e) {
+    } catch (e) {
       if (e is DioError) {
         return Left(ServerFailure.fromDioError(e));
       }
@@ -32,8 +32,22 @@ class HomeRepoImpl extends HomeRepo {
   }
 
   @override
-  Future<List<BookModel>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return Right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
   }
 }
