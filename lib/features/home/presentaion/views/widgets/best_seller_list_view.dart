dd0b6848884aea6db5c456b88b405978c5abdae1
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2025-12-17 18:20:35 +0200

    display newest books

--- lib/features/home/presentaion/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentaion/views/widgets/best_seller_list_view.dart
@@ -1,20 +1,34 @@
+import 'package:bookly/core/widgets/custom_errors_widget.dart';
+import 'package:bookly/core/widgets/custom_loading_indecator.dart';
+import 'package:bookly/features/home/presentaion/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/features/home/presentaion/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BestSellerListView extends StatelessWidget {
   const BestSellerListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0),
-          child: const BookListViewItem(),
-        );
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccess) {
+          return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: BookListViewItem(bookModel: state.books[index]),
+              );
+            },
+          );
+        } else if (state is NewestBooksFailure) {
+          return CustomErrorsWidget(errorMessage: state.errMessage);
+        } else {
+          return CustomLoadingIndecator();
+        }
       },
     );
   }
--- lib/features/home/presentaion/views/widgets/book_list_view_item.dart
+++ lib/features/home/presentaion/views/widgets/book_list_view_item.dart
@@ -1,14 +1,15 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/presentaion/views/widgets/book_rating.dart';
+import 'package:bookly/features/home/presentaion/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class BookListViewItem extends StatelessWidget {
-  const BookListViewItem({super.key});
- 
+  const BookListViewItem({super.key, required this.bookModel});
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -19,18 +20,8 @@ class BookListViewItem extends StatelessWidget {
         children: [
           SizedBox(
             height: 125,
-            child: AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AssetsData.testImage),
-                  ),
-                ),
-              ),
+            child: CustomBookImage(
+              imageUrl: bookModel.volumeInfo.imageLinks?.thumbnail ?? '',
             ),
           ),
           const SizedBox(width: 30),
@@ -41,7 +32,7 @@ class BookListViewItem extends StatelessWidget {
                 SizedBox(
                   width: MediaQuery.of(context).size.width * .5,
                   child: Text(
-                    'Harry Poter and the Goblet of Fire',
+                    bookModel.volumeInfo.title!,
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
 
@@ -51,19 +42,25 @@ class BookListViewItem extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 3),
-                const Text("J.K. Rowling", style: Styles.textStyle14),
+                Text(
+                  bookModel.volumeInfo.authors![0],
+                  style: Styles.textStyle14,
+                ),
                 const SizedBox(height: 3),
 
                 Row(
                   children: [
                     Text(
-                      "19.99",
+                      "Free",
                       style: Styles.textStyle20.copyWith(
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     const Spacer(),
-                    const BookRating(),
+                    BookRating(
+                      rating: bookModel.volumeInfo.averageRating?.toInt() ?? 0,
+                      count: bookModel.volumeInfo.ratingsCount ?? 0,
+                    ),
                   ],
                 ),
               ],
