aa9778f783a554b534df5b8faa29a568cefa25ca
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2025-12-19 15:27:06 +0200

    2

--- lib/core/widgets/custom_errors_widget.dart
+++ lib/core/widgets/custom_errors_widget.dart
@@ -7,6 +7,12 @@ class CustomErrorsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(errorMessage, style: Styles.textStyle18));
+    return Center(
+      child: Text(
+        errorMessage,
+        style: Styles.textStyle18,
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
